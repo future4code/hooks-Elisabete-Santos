b51c1aa006c30d23e8059a7b42173579eda2b2e2
Liz alves <elisabete_carente@hotmail.com>
2022-06-14 17:40:46 -0300

    esqueleto do projeto

---
+++ modulo4/projeto-labex/labex/src/pages/AdminHomePage.js/AdminHomePage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AdminHomePage = () => {
+   return (
+      <div>
+         Para o administrador ver a lista de viagens e poder deletá-las ou acessar o detalhe de cada uma delas
+      </div>
+   )
+}
+
+export default AdminHomePage
---
+++ modulo4/projeto-labex/labex/src/pages/LoginPage/LoginPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoginPage = () => {
+   return (
+      <div>
+         Para fazermos login como administrador
+      </div>
+   )
+}
+
+export default LoginPage
