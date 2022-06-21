84df794344c41fe626532a58ea5c0ddebf20f928
Liz alves <elisabete_carente@hotmail.com>
2022-06-21 13:26:14 -0300

    autenticando a rota de login e colocando as autorizoes

--- modulo4/projeto-labex/labex/src/pages/LoginPage/LoginPage.js
+++ modulo4/projeto-labex/labex/src/pages/LoginPage/LoginPage.js
@@ -1,15 +1,46 @@
-import React from 'react'
+import React, { useEffect, useState } from "react"
+import axios from "axios"
 import { useNavigate } from "react-router-dom"
+import { BASE_URL } from "../../constants/Base_url";
 import { goBack, goToListTrip, goHome } from "../../routes/Coordinator";
 import { Header } from '../../components/Header/Header'
+import { MainForm, ButtonDiv } from "./styled"
 
 const LoginPage = () => {
 
-   const navigate = useNavigate()
+   const navigate = useNavigate();
+   const [email, setEmail] = useState("");
+   const [senha, setSenha] = useState("");
+
+   const onChangeEmail = (event) => {
+      setEmail(event.target.value)
+   }
+
+   const onChangeSenha = (event) => {
+      setSenha(event.target.value)
+   }
 
    const onSubmitLogin = () => {
-
+      const body = {
+         email: email,
+         password: senha,
+      };
+      axios
+         .post(`${BASE_URL}/login`, body)
+         .then((response) => {
+            navigate("/admin/trips/list");
+            localStorage.setItem("token", response.data.token);
+         })
+         .catch((error) => {
+            alert("Login ou senha não autorizado");
+         })
    }
+
+   useEffect(() => {
+      if (localStorage.getItem("token")) {
+         navigate("/admin/trips/list");
+      }
+   })
 
    return (
       <div>
@@ -17,9 +48,26 @@ const LoginPage = () => {
             first={{ function: goToListTrip, text: "Início" }}
             second={{ function: goHome, text: "Viagens" }}
          />
-         <p>Para fazermos login como administrador</p>
-         <button onClick={onSubmitLogin}>ENTRAR</button>
-         <button onClick={() => goBack(navigate)}>VOLTAR</button>
+         <MainForm>
+            <h2>Área Administrativa</h2>
+            <input
+               placeholder="Login"
+               type="email"
+               value={email}
+               onChange={onChangeEmail}
+            />
+
+            <input
+               placeholder="Senha"
+               type="senha"
+               value={senha}
+               onChange={onChangeSenha}
+            />
+            <ButtonDiv>
+               <button onClick={onSubmitLogin}>ENTRAR</button>
+               <button onClick={() => goBack(navigate)}>VOLTAR</button>
+            </ButtonDiv>
+         </MainForm>
       </div>
    )
 }
--- modulo4/projeto-labex/labex/src/pages/TripDetailsPage/TripDetailsPage.js
+++ modulo4/projeto-labex/labex/src/pages/TripDetailsPage/TripDetailsPage.js
@@ -1,15 +1,59 @@
-import React from 'react'
+import axios from "axios"
+import React, { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+import { BASE_URL } from "../../constants/Base_url"
+import { useProtectedPage } from "../../hooks/useRequestData"
 import { logout, goBack } from "../../routes/Coordinator"
 import { Header } from '../../components/Header/Header'
+import { Lista, CardViagem } from "./styled"
 
 const TripDetailsPage = () => {
+
+   useProtectedPage();
+   const token = localStorage.getItem("token");
+   const HEADERS = {
+      headers: {
+         auth: token,
+      },
+   }
+
+   const { id } = useParams();
+   const [detailsTrip, setDetailsTrip] = useState('')
+   const [update, setUpdate] = React.useState(false)
+
+   useEffect(() => {
+
+      axios.get(`${BASE_URL}/trip/${id}`, HEADERS)
+         .then((response) => {
+            setDetailsTrip(response.data)
+            setUpdate(!update)
+         })
+         .catch((error) => {
+            alert(error.response)
+         })
+   }, [update])
+
+
+
+
    return (
       <div>
          <Header
             first={{ function: goBack, text: "Voltar" }}
             second={{ function: logout, text: "Sair" }}
          />
-         <p>Para o administrador ver o detalhe de uma viagem específica, bem como os candidatos que aplicaram para ela</p>
+         <Lista>
+            {detailsTrip && (
+               <CardViagem>
+                  <p>Nome: {detailsTrip.trip.name}</p>
+                  <p>Planeta: {detailsTrip.trip.planet}</p>
+                  <p>Descrição: {detailsTrip.trip.description}</p>
+                  <p> Data: {detailsTrip.trip.date}</p>
+                  <p> Duração: {detailsTrip.trip.durationInDays}</p>
+               </CardViagem>
+            )}
+         </Lista>
+
       </div>
    )
 }
