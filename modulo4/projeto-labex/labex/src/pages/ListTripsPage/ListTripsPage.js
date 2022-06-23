3b40614dcf1eb4381cf4c58c813b06bffc06d127
Liz alves <elisabete_carente@hotmail.com>
2022-06-23 16:51:17 -0300

    adicionando as imagens do projeto

--- modulo4/projeto-labex/labex/src/pages/ListTripsPage/ListTripsPage.js
+++ modulo4/projeto-labex/labex/src/pages/ListTripsPage/ListTripsPage.js
@@ -1,20 +1,50 @@
-import React from 'react'
+import * as React from "react"
 import { useNavigate } from "react-router-dom"
-import { goToApplicationForm, goBack, goHome, goToLoignPage } from "../../routes/Coordinator"
+import { goToApplicationForm, goHome, goToLoignPage } from "../../routes/Coordinator"
+import { BASE_URL } from "../../constants/Base_url"
+import { useRequestData } from "../../hooks/useRequestData"
+import { Lista, CardName, Div } from "./styled";
+import Card from "@mui/material/Card";
 import { Header } from '../../components/Header/Header'
+import { IoMdPlanet } from "react-icons/io"
 
 const ListTripsPage = () => {
 
    const navigate = useNavigate()
+   const listTrip = useRequestData(`${BASE_URL}/trips`, {})
+
+
+   const trips =
+      listTrip.trips &&
+      listTrip.trips.map((trip) => {
+         return (
+
+            <div key={trip.id}>
+               <Card className="DivCard" color='primary' sx={{ minWidth: 275 }}>
+
+                  <CardName> <IoMdPlanet /> {trip.planet}</CardName>
+                  <Div>
+                     <p className="top-top">Nome: {trip.name}</p>
+                     <p className="top">Descrição: {trip.description}</p>
+                     <p className="top1"> Duração: {trip.durationInDays} dias</p>
+                     <p className="top2"> Data: {trip.date}</p>
+                     <button className="button" onClick={() => goToApplicationForm(navigate, trip.id)}>
+                        Inscrever-se
+                     </button>
+                  </Div>
+               </Card>
+            </div>
+         );
+      })
 
    return (
       <div>
          <Header
-            first={{ function: goHome, text: "Inicio", }}
+            first={{ function: goHome, text: "Home", }}
             second={{ function: goToLoignPage, text: "Login", }}
          />
-         <p>Para vermos todas as viagens</p>
-         <button onClick={() => goBack(navigate)}>Voltar</button>
+         <Lista>{trips}</Lista>
+
       </div>
    )
 }
