185a58dc98b458af93f6bf730c9ffc9ea1a30510
Liz alves <elisabete_carente@hotmail.com>
2022-06-23 15:47:11 -0300

    adicionando o hooks useForm e terminando a pagina de login

--- modulo4/projeto-labex/labex/src/pages/LoginPage/LoginPage.js
+++ modulo4/projeto-labex/labex/src/pages/LoginPage/LoginPage.js
@@ -1,58 +1,58 @@
-import React, { useEffect, useState } from "react"
 import axios from "axios"
+import React, { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
-import { BASE_URL } from "../../constants/Base_url";
-import { goBack, goToListTrip, goHome } from "../../routes/Coordinator";
+import { BASE_URL } from "../../constants/Base_url"
+import { goBack, goToListTrip, goHome } from "../../routes/Coordinator"
 import { Header } from '../../components/Header/Header'
 import { MainForm, ButtonDiv } from "./styled"
 import { IoIosRocket } from "react-icons/io"
 
-
 const LoginPage = () => {
 
    const navigate = useNavigate();
-   const [email, setEmail] = useState("");
-   const [senha, setSenha] = useState("");
+   const [email, setEmail] = useState("")
+   const [senha, setSenha] = useState("")
 
    const onChangeEmail = (event) => {
-      setEmail(event.target.value)
-   }
+      setEmail(event.target.value);
+   };
 
    const onChangeSenha = (event) => {
-      setSenha(event.target.value)
-   }
+      setSenha(event.target.value);
+   };
 
    const onSubmitLogin = () => {
+
       const body = {
          email: email,
          password: senha,
       };
       axios
          .post(`${BASE_URL}/login`, body)
-         .then((response) => {
-            navigate("/admin/trips/list");
-            localStorage.setItem("token", response.data.token);
+         .then((res) => {
+            localStorage.setItem("token", res.data.token);
+            navigate("/admin/trips/list")
          })
-         .catch((error) => {
+         .catch((err) => {
             alert("Login ou senha não autorizado");
-         })
-   }
+         });
+   };
 
    useEffect(() => {
       if (localStorage.getItem("token")) {
          navigate("/admin/trips/list");
       }
-   })
+   });
 
    return (
       <div>
          <Header
             first={{ function: goToListTrip, text: "Viagens" }}
-            second={{ function: goHome, text: "Início" }}
+            second={{ function: goHome, text: "Home" }}
          />
          <MainForm>
-            <IoIosRocket />
-            <h1>LOGI<span>N</span></h1>
+
+            <h1><IoIosRocket /> LOGI<span>N</span></h1>
             <h2>Área Administrativa</h2>
             <input
                placeholder="Login"
