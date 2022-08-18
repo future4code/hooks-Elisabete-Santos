9a1842326bb9a5ecddd7dff4894a310e7bc994f5
Liz alves <elisabete_carente@hotmail.com>
2022-08-18 15:22:01 -0300

    exercicio aprofundamento express

---
+++ modulo5/aprofundamento-express/src/data.ts
@@ -0,0 +1,71 @@
+// Exercício 2
+type Task = {
+   userId: number,
+   id: number,
+   title: string,
+   completed: boolean
+}
+
+// Exercício 3 
+export const toDoList: Task[] = [
+   {
+      userId: 1,
+      id: 1,
+      title: "Estudar",
+      completed: true,
+    },
+    {
+      userId: 1,
+      id: 2,
+      title: "Faxina",
+      completed: false,
+    },
+    {
+      userId: 1,
+      id: 4,
+      title: "Ler",
+      completed: true,
+    },
+    {
+      userId: 2,
+      id: 35,
+      title: "Lavar roupa",
+      completed: true,
+    },
+    {
+      userId: 2,
+      id: 39,
+      title: "Ir as compras",
+      completed: false,
+    },
+    {
+      userId: 2,
+      id: 40,
+      title: "Cozinhar",
+      completed: true,
+    },
+    {
+      userId: 3,
+      id: 43,
+      title: "Ir ao cinena",
+      completed: true,
+    },
+    {
+      userId: 3,
+      id: 44,
+      title: "Programar",
+      completed: true,
+    },
+    {
+      userId: 3,
+      id: 45,
+      title: "Ir ao supermercado",
+      completed: false,
+    },
+    {
+      userId: 4,
+      id: 48,
+      title: "Show",
+      completed: false,
+    }
+] 
