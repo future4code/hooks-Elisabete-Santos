0ae89ce2d4ac38a67f9dc8ba4e3ad4c35d632df1
Liz alves <elisabete_carente@hotmail.com>
2022-12-09 08:54:03 -0300

    estrutura do projeto

--- rodada-cases/case-amaro/tables.sql
+++ rodada-cases/case-amaro/tables.sql
@@ -1,48 +1,22 @@
 CREATE TABLE
-    cookenu_users (
-        id VARCHAR(255) PRIMARY KEY UNIQUE NOT NULL,
+    amaro_users (
+        id VARCHAR(80) PRIMARY KEY,
         name VARCHAR(255) NOT NULL,
         email VARCHAR(255) UNIQUE NOT NULL,
-        password VARCHAR(255) NOT NULL
+        password VARCHAR(255) NOT NULL,
+        role VARCHAR(7) DEFAULT "NORMAL"
     );
 
 CREATE TABLE
-    cookenu_recipes (
-        id VARCHAR(255) PRIMARY KEY UNIQUE NOT NULL,
-        title VARCHAR(255) NOT NULL,
-        description VARCHAR(8191) NOT NULL,
-        user_id VARCHAR(255) NOT NULL,
-        FOREIGN KEY(user_id) REFERENCES cookenu_users(id),
-        creation_date DATE NOT NULL
+    amaro_products(
+        id VARCHAR(80) PRIMARY KEY,
+        name VARCHAR(100) NOT NULL
     );
 
 CREATE TABLE
-    cookenu_followers (
-        id VARCHAR(255) PRIMARY KEY UNIQUE NOT NULL,
-        follower_id VARCHAR(255) NOT NULL,
-        FOREIGN KEY(follower_id) REFERENCES cookenu_users(id),
-        followed_id VARCHAR(255) NOT NULL,
-        FOREIGN KEY(followed_id) REFERENCES cookenu_users(id)
+    amaro_tags(
+        tag_id VARCHAR(80) PRIMARY KEY,
+        tag VARCHAR(40),
+        product_id VARCHAR(80) NOT NULL,
+        FOREIGN KEY (product_id) REFERENCES amaro_products(id)
     );
-
-ALTER TABLE cookenu_users
-ADD
-    role ENUM("normal", "admin") DEFAULT "normal";
-
--- SELECT *
-
--- FROM cookenu_followers
-
---     JOIN cookenu_users ON cookenu_followers.followed_id = cookenu_users.id --
-
---     JOIN cookenu_recipes ON cookenu_recipes.user_id = cookenu_recipes.user_id
-
--- WHERE
-
---     cookenu_followers.follower_id = "7d392a57-3f9b-4b6c-8e46-b9d7c2238797";
-
--- SELECT *
-
--- FROM cookenu_recipes
-
---     JOIN cookenu_followers ON cookenu_recipes.user_id = cookenu_recipes.user_id
